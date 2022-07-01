613ceb1594b6a3ef102e249b7fd9af519d9e0444
vodjanoj <vladimirs.vodjanojs@gmail.com>
2022-07-01 11:37:57 +0300

    React Data Fetching

--- client/src/components/JobBoard.js
+++ client/src/components/JobBoard.js
@@ -1,10 +1,20 @@
 import JobList from "./JobList";
-import { jobs } from "../fake-data";
 import { getJobs } from "../graphql/queries";
-
-getJobs();
+import { useEffect, useState } from "react";
 
 function JobBoard() {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    console.log('mounted')
+    // "getJobs" returns a Promise,  usually I use "await" to unpack a Promise, but
+    // unfortunately we cannot declare this function as "async", because the function we pass to useEffect cannot
+    // return anything, but an "async" function always returns a Promise, So instead I'm going to call "then" on the Promise,
+    // getJobs().then(jobs => setJobs(jobs)) below is how we can simplify the code
+    getJobs().then(setJobs)
+  }, []);
+
+  console.log('[JobBoard] jobs', jobs)
   return (
     <div>
       <h1 className="title">Job Board</h1>
