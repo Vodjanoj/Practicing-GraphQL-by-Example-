e3fb5cc0a2a930607d9fe7970b914253f6569456
vodjanoj <vladimirs.vodjanojs@gmail.com>
2022-07-04 13:43:11 +0300

    Company-Jobs Association

--- client/src/components/CompanyDetail.js
+++ client/src/components/CompanyDetail.js
@@ -1,6 +1,7 @@
 import { useParams } from "react-router";
 import { useEffect, useState } from "react";
 import { getCompany } from "../graphql/queries";
+import JobList from "./JobList";
 
 function CompanyDetail() {
   const [company, setCompany] = useState(null);
@@ -12,15 +13,15 @@ function CompanyDetail() {
     getCompany(companyId).then(setCompany);
   }, [companyId]);
 
-  console.log("company", company);
-
   if (!company) {
-    return <p>Loading...</p>
+    return <p>Loading...</p>;
   }
   return (
     <div>
       <h1 className="title">{company.name}</h1>
       <div className="box">{company.description}</div>
+      <h5 classname="title is-5">Jobs at {company.name}</h5>
+      <JobList jobs={company.jobs} />
     </div>
   );
 }
