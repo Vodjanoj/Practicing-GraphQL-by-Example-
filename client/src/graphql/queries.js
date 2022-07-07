c2190b85f93f6a29130b01f6e0d0e3d23af66bb4
vodjanoj <vladimirs.vodjanojs@gmail.com>
2022-07-07 12:08:16 +0300

    Cache Manipulation

--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -9,14 +9,34 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
+const JOB_QUERY = gql`
+  query JobQuery($id: ID!) {
+    job(id: $id) {
+      id
+      title
+      company {
+        id
+        name
+      }
+      description
+    }
+  }
+`;
+
 export async function createJob(input) {
   const mutation = gql`
     mutation CreateJobMutation($input: CreateJobInput!) {
       # job is an alias
       job: createJob(input: $input) {
-        # since we're only using the job ID from the response, we could actually remove
-        # all these other fields. This way we reduce the size of the response a little bit.
+        # we need also all these data to store in the cache and then use the data
+        # to display in the component
         id
+        title
+        company {
+          id
+          name
+        }
+        description
       }
     }
   `;
@@ -24,9 +44,27 @@ export async function createJob(input) {
   const variables = { input };
 
   const context = { headers: { Authorization: "Bearer " + getAccessToken() } };
+  // update updates data in the cache
+  // "result" is the same object we get when we call "client.mutate", so it contains the
+  // data in the GraphQL response, therefore we can destructure that "result" object here in the same way.
+  // { data: {job} }
+  // update - so this is how we can write some data directly into the cache.
+  // We are effectively storing the same data that is normally fetched by the "job" query, but in this
+  // case we're doing it manually, after a "createJob" mutation.
   const {
     data: { job },
-  } = await client.mutate({ mutation, variables, context });
+  } = await client.mutate({
+    mutation,
+    variables,
+    context,
+    update: (cache, { data: { job } }) => {
+      cache.writeQuery({
+        query: JOB_QUERY,
+        variables: { id: job.id },
+        data: { job },
+      });
+    },
+  });
 
   return job;
 }
@@ -67,30 +105,16 @@ export async function getCompany(id) {
   const variables = { id };
   const {
     data: { company },
-  } = await client.query({ query, variables });
+  } = await client.query({ query , variables });
   // const { company } = await request(GRAPHQL_URL, query, variables);
   return company;
 }
 
 export async function getJob(id) {
-  const query = gql`
-    query JobQuery($id: ID!) {
-      job(id: $id) {
-        id
-        title
-        company {
-          id
-          name
-        }
-        description
-      }
-    }
-  `;
-
   const variables = { id };
   const {
     data: { job },
-  } = await client.query({ query, variables });
+  } = await client.query({ query: JOB_QUERY, variables });
 
   // const { job } = await request(GRAPHQL_URL, query, variables);
   return job;
